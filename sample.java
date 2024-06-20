8719511a73150b0e4404f41861e9efcdbd79d152
Rushi457 <rushikeshj457@gmail.com>
2024-06-20 17:42:31 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,1 +1,21 @@
-hello sample
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="easy-qrcode.js"></script>
+    <title>QR Code Generator Website</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-size: 16px;
+            background: #fff;
+            font-family: Verdana, Tahoma, Arial;
+        }
+
+        .container {
+            position: relative;
+            width: 70%;
+
